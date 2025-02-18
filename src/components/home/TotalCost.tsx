758873f8683dd5094fa534638a02c303af0bdd93
938938 <938kjh@gmail.com>
2025-02-19 01:14:08 +0900

    [FEAT] 총합계 로직 구현

--- src/components/home/TotalCost.tsx
+++ src/components/home/TotalCost.tsx
@@ -1,5 +1,14 @@
+'use client';
+
+import { useRecoilValue } from 'recoil';
+import { listState } from 'src/recoil/listState';
+
 const TotalCost = () => {
-  return <div>총액 : 00000원</div>;
+  const goodsList = useRecoilValue(listState);
+  const totalCost = goodsList.reduce((sum, item) => {
+    return item.count > 0 ? sum + item.cost * item.count : sum;
+  }, 0);
+  return <div>총액 : {totalCost.toLocaleString()}원</div>;
 };
 
 export default TotalCost;
