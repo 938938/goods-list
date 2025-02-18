bfe0d55346b3e20f005b1218b6742dc37608e11a
938938 <938kjh@gmail.com>
2025-02-19 01:09:03 +0900

    [FEAT] 갯수변경 로직 구현

--- src/components/home/Item.tsx
+++ src/components/home/Item.tsx
@@ -1,14 +1,24 @@
 'use client';
 
 import { Input } from '@material-tailwind/react';
-import { useState } from 'react'
+import { useState } from 'react';
+import { useSetRecoilState } from 'recoil';
 import { ItemType } from 'src/model/type';
+import { listState } from 'src/recoil/listState';
 
 const Item = ({ item }: { item: ItemType }) => {
   const [count, setCount] = useState<number>(item.count);
+  const setGoodsList = useSetRecoilState(listState);
 
   const onChangeHandler = (e) => {
-    setCount(Number(e.target.value));
+    let newCount = Number(e.target.value);
+    if (isNaN(newCount) || newCount < 0) newCount = 0;
+    setCount(newCount);
+    setGoodsList((prev) =>
+      prev.map((ele) =>
+        ele.id === item.id ? { ...ele, count: newCount } : ele
+      )
+    );
   };
 
   return (
@@ -16,7 +26,7 @@ const Item = ({ item }: { item: ItemType }) => {
       <p>
         {item.name} - {item.cost.toLocaleString()}원 / {item.count} 개
       </p>
-      <Input type='number' value={count} onChange={onChangeHandler} />
+      <Input type='number' value={count} onChange={onChangeHandler} min={0} />
     </li>
   );
 };
