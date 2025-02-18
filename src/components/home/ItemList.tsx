55b148abe07ca4078e5217bdfebd044862467d04
938938 <938kjh@gmail.com>
2025-02-18 23:25:38 +0900

    [FEAT] atom 생성

--- src/components/home/ItemList.tsx
+++ src/components/home/ItemList.tsx
@@ -1,15 +1,20 @@
 'use client';
 
+import { atom } from 'recoil';
 import Item from './Item';
 
 const ItemList = () => {
+  const listState = atom({
+    key: 'list',
+    default: [],
+  });
+  console.log(listState);
   return (
     <div>
-      {/* {listQuery.isPending && <p>Loading...</p>}
       <ul>
-        {listQuery.data &&
-          listQuery.data.map((item) => <Item item={item} key={item.id} />)}
-      </ul> */}
+        {/* {listState &&
+          listState.map((item) => <Item item={item} key={item.id} />)} */}
+      </ul>
     </div>
   );
 };
