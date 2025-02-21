5942beb9bb9835f431838c862284646095b8db8f
938938 <938kjh@gmail.com>
2025-02-21 21:55:43 +0900

    [FEAT] 선택물품 표시 기능 구현

--- src/components/home/TotalCost.tsx
+++ src/components/home/TotalCost.tsx
@@ -9,8 +9,18 @@ const TotalCost = () => {
   const totalCost = goodsList.reduce((sum, item) => {
     return item.count > 0 ? sum + item.cost * item.count : sum;
   }, 0);
+  const checkedItem = goodsList.filter((item) => item.count !== 0);
+  console.log(checkedItem);
+
   return (
-    <div className='flex py-3 justify-center'>
+    <div className='flex py-3 justify-center flex-col items-center'>
+      <div className='flex gap-3'>
+        {checkedItem.map((item) => (
+          <p key={item.name}>
+            {item.name} x {item.count}개
+          </p>
+        ))}
+      </div>
       <Typography color='black' variant='lead'>
         총액 : {totalCost.toLocaleString()}원
       </Typography>
